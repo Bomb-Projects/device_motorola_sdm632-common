0f1a7b15c219686bac49e2ec1eaf5a537f2e3bff
ROMSG <romsg85@gmail.com>
2023-09-16 22:28:50 -0700

    sdm632-common: Set the compiler filter for shared apks to quicken
    Change-Id: I8ed63116b929920f604cd1d64110fef95744e425

--- properties.mk
+++ properties.mk
@@ -72,6 +72,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.enable_default_color_mode=1
 
+# Dex
+PRODUCT_PROPERTY_OVERRIDES += \
+    pm.dexopt.shared=quicken
+
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.hwc_set_default_colormode=true \
