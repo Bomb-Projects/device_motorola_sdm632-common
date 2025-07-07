4a2bcf8940685e73381ffe13b0c0a474b5c3876b
L1ghtzin <lightzin021@gmail.com>
2025-07-06 22:02:57 -0300

    sdm632-common: Build kernel with clang 11

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -172,6 +172,9 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
+TARGET_KERNEL_CLANG_VERSION := clang-r383902
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-r383902
+
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 
